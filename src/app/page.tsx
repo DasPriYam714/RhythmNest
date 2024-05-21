cbaa738033157ec588b8288ed4fc39d734f7d2fc
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-05-22 02:01:07 +0600

    features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import CoverSection from "@/components/CoverSection";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col  justify-between p-10 bg-black">
       <div className="dark">
         <CoverSection></CoverSection>
+      </div>
+      <div>
+        <Features></Features>
       </div>
       
     </main>
